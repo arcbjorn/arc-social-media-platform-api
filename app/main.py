720cd25c6c9abe04a412422596aeecb2c75674dd
arcbjorn <arky.happ@gmail.com>
2021-11-07 00:39:01 +0300

    chore (workspace): format with flake8 & black

--- app/main.py
+++ app/main.py
@@ -1,15 +1,12 @@
-from os import stat
 from typing import Optional
 from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
-from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 
-from starlette import status
-
 app = FastAPI()
+
 
 class Post(BaseModel):
     title: str
@@ -17,9 +14,16 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+
 while True:
     try:
-        conn = psycopg2.connect(host='localhost', database='social_media_platform',user='postgres', password='postgres', cursor_factory=RealDictCursor)
+        conn = psycopg2.connect(
+            host="localhost",
+            database="social_media_platform",
+            user="postgres",
+            password="postgres",
+            cursor_factory=RealDictCursor,
+        )
         cursor = conn.cursor()
         print("Database connection was succesfull")
         break
@@ -30,22 +34,26 @@ while True:
 
 my_posts = [
     {"title": "title1", "content": "content of post 1", "id": 1},
-    {"title": "title2", "content": "content of post 2", "id": 2}
+    {"title": "title2", "content": "content of post 2", "id": 2},
 ]
+
 
 def find_post(id):
     for p in my_posts:
         if p["id"] == id:
             return p
 
+
 def find_index_post(id):
     for i, p in enumerate(my_posts):
         if p["id"] == id:
             return i
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
 @app.get("/posts")
 async def get_posts():
@@ -53,46 +61,60 @@ async def get_posts():
     posts = cursor.fetchall()
     return {"data": posts}
 
+
 @app.post("/posts")
 async def create_post(post: Post, status_code=status.HTTP_201_CREATED):
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) """,
-                   (post.title, post.content, post.published))
+    cursor.execute(
+        """INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) """,
+        (post.title, post.content, post.published),
+    )
     new_post = cursor.fetchone()
-    
+
     conn.commit()
-    
+
     return {"data": new_post}
+
 
 @app.get("/posts/{id}")
 async def get_post(id: int):
     cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id)))
     post = cursor.fetchone()
     if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id: {id} was not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} was not found",
+        )
     return {"post_details": post}
+
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_post(id: int):
     cursor.execute("""DELETE FROM posts WHERE id = %s returning *""", (str(id)))
     deleted_post = cursor.fetchone()
     conn.commit()
-    
+
     if deleted_post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id: {id} does not exist")
-    
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} does not exist",
+        )
+
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
 
 @app.put("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def update_post(id: int, post: Post):
-    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
-                          (post.title, post.content, post.published, str(id)))
+    cursor.execute(
+        """UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
+        (post.title, post.content, post.published, str(id)),
+    )
     updated_post = cursor.fetchone()
     conn.commit()
-    
+
     if updated_post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id: {id} does not exist")
-    
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} does not exist",
+        )
+
     return {"data": updated_post}
