bc3cb9ffd41b111eb268b6875af1be09abff7071
arcbjorn <arky.happ@gmail.com>
2021-11-07 01:14:11 +0300

    feat (app:models.Post): add created_at

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
+from sqlalchemy.sql.expression import text
 from .database import Base
 from sqlalchemy import Column
-from sqlalchemy.sql.sqltypes import Integer, String, Boolean
+from sqlalchemy.sql.sqltypes import TIMESTAMP, Integer, String, Boolean
 
 
 class Post(Base):
@@ -10,3 +11,6 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default="TRUE", nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
