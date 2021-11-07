e3dc0fea4419cb641c89b615fe34fc9c7aa0f02e
arcbjorn <arky.happ@gmail.com>
2021-11-07 16:53:48 +0300

    feat (app:schemas): add User response, Create

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
 from datetime import datetime
 
+from pydantic.networks import EmailStr
+
 
 class PostBase(BaseModel):
     title: str
@@ -16,8 +18,23 @@ class PostUpdate(PostBase):
     pass
 
 
+# response Post model
 class Post(PostBase):
     id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode: True
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class User(BaseModel):
+    id: int
+    email: EmailStr
     created_at: datetime
 
     class Config:
