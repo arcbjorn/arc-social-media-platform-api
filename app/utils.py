31427a5b83b590e74b1817c1f84e74d41989974f
arcbjorn <arky.happ@gmail.com>
2021-11-07 19:41:07 +0300

    feat (app:utils): add verify password method

--- app/utils.py
+++ app/utils.py
@@ -5,3 +5,7 @@ pwd_context = CryptoContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash(password: str):
     return pwd_context.hash(password)
+
+
+def verify(plain_password: str, hashed_passwod: str):
+    return pwd_context.verify(plain_password, hashed_passwod)
