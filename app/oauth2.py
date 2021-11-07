feb0506731db545aa349dc7272ef5ddabac678f0
arcbjorn <arky.happ@gmail.com>
2021-11-07 21:15:25 +0300

    feat(app:oauth2):add create&verify token, get user

---
+++ app/oauth2.py
@@ -0,0 +1,60 @@
+from fastapi.exceptions import HTTPException
+from fastapi.param_functions import Depends
+from jose import jwt
+from datetime import datetime, timedelta
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import status
+from jose.exceptions import JWTError
+from sqlalchemy.orm.session import Session
+
+from app import models, schemas, database
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+SECRET_KEY = "5e805ee91b5e1df779697e2e0883eca29b7d593e0e16933083b5dac0f217674b"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
+
+        if id is None:
+            raise credentials_exception
+
+        token_data = schemas.TokenData(id=id)
+
+    except JWTError:
+        raise credentials_exception
+
+    return token_data
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(database.database.get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    token = verify_access_token(token, credentials_exception)
+
+    user = db.query(models.User.id).filter(models.User.id == token.id).first()
+
+    return user
