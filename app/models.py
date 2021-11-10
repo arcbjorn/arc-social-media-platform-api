fc26c17499747ea13b31ff89a28d6d9ae3653433
arcbjorn <arky.happ@gmail.com>
2021-11-11 00:09:21 +0300

    feat (app.models): add Post - User relationship

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.schema import ForeignKey
 from .database import Base
 from sqlalchemy import Column
 from sqlalchemy.sql.sqltypes import TIMESTAMP, Integer, String, Boolean
@@ -11,6 +13,11 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default="TRUE", nullable=False)
+
+    owner = relationship("User")
+    owner_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
 
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
