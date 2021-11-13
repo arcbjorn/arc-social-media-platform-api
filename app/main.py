a7c99b66dd026fd0076b9f675b1a17e9f82d7815
arcbjorn <arky.happ@gmail.com>
2021-11-13 14:50:07 +0300

    feat (app.main): connect vote router

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from app import models
 from app.database import engine
-from app.routers import post_router, user_router, auth_router
+from app.routers import post_router, user_router, auth_router, vote_router
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -11,3 +11,4 @@ app = FastAPI()
 app.include_router(post_router.router)
 app.include_router(user_router.router)
 app.include_router(auth_router.router)
+app.include_router(vote_router.router)
