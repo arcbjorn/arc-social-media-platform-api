641d554747bf7703fa8b000781b58e7e53c14fb5
arcbjorn <arky.happ@gmail.com>
2021-11-14 23:02:43 +0300

    feat (app.config): add defaults

--- app/config.py
+++ app/config.py
@@ -2,10 +2,10 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    db_hostname: str
-    db_port: str
-    db_password: str
-    db_name: str
+    db_hostname: str = "localhost"
+    db_port: str = "5432"
+    db_password: str = "password"
+    db_name: str = "postgres"
     db_username: str = "postgres"
 
     secret_key: str = "23wetrhdfjnw4543"
